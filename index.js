241a0c06b1288cec4ea54faa6542aac97b758f80
MrCoderBoy345 <kaarthik.hari@gmail.com>
2020-04-29 23:25:23 -0400

    rewrite with both async/await and promises

--- index.js
+++ index.js
@@ -7,58 +7,82 @@ const TBAKey = process.env.TBAKEY
 
 const app = express()
 const pool = new Pool({
-  connectionString: process.env.CONNECTIONSTRING
+  connectionString: process.env.CONNECTIONSTRING,
+  max: 1,
 })
 
 app.use(express.json(), cors(), express.urlencoded({ extended: true }))
 
-var server = app.listen(process.env.PORT || 3000, function () {
-  var port = server.address().port
+const server = app.listen(process.env.PORT || 3000, function () {
+  const port = server.address().port
   console.log("App now running on port", port)
 })
 
 app.get("/events", function (req, response) {
   pool
     .connect()
-    .then(client => {
-      return client
-        .query("SELECT * FROM event")
-        .then(result => {
-          client.release()
-          response.json(result.rows)
-        })
+    .then(async client => {
+      try {
+        const result = await client.query("SELECT * FROM event")
+        response.json(result.rows)
+      } finally {
+        client.release()
+      }
     })
     .catch(e => console.error(e.stack))
-})
+}) 
+
 app.get("/matches/:eventid", function (req, response) {
   const eventid = req.params.eventid
   pool
     .connect()
-    .then(client => {
-      return client
-        .query("SELECT match.* FROM match INNER JOIN event ON match.eventid = event.id WHERE event.blue_alliance_id = $1", [eventid])
-        .then(result => {
-          client.release()
-          response.json(result.rows)
-        })
+    .then(async client => {
+      try {
+        const result = await client.query("SELECT match.* FROM match INNER JOIN event ON match.eventid = event.id WHERE event.blue_alliance_id = $1", [eventid])
+        response.json(result.rows)
+      } finally {
+        client.release()
+      }
     })
     .catch(e => console.error(e.stack))
 })
-app.get("/scout/", function (req, response) {
-  const matchid = parseInt(req.headers.matchid)
-  const teamNumber = parseInt(req.headers.team_number)
+
+app.get("/scout/:teamNumber/:matchid", function (req, response) {
+  const matchid = parseInt(req.params.matchid)
+  const teamNumber = parseInt(req.params.teamNumber)
   pool
     .connect()
-    .then(client => {
-      return client
-        .query(
-          "SELECT team_match_stat.* FROM match INNER JOIN team_match_stat ON match.id = team_match_stat.matchid WHERE matchid = $1 AND team_number = $2",
-          [matchid, teamNumber]
-        )
-        .then(result => {
-          client.release()
-          response.json(result.rows)
-        })
+    .then(async client => {
+      try {
+        const result = await client.query("SELECT * FROM team_match_stat WHERE matchid = $1 AND team_number = $2", [matchid, teamNumber])
+        response.json(result.rows[0])
+      } finally {
+        client.release()
+      }
+    })
+    .catch(e => console.error(e.stack))
+})
+
+app.post("/scout/:teamNumber/:matchid", function (req, response) {
+  const matchid = parseInt(req.params.matchid)
+  const teamNumber = parseInt(req.params.teamNumber)
+  const data = req.body
+  pool
+    .connect()
+    .then(async client => {
+      try {
+        const result = await client.query("SELECT COUNT(*) FROM team_match_stat WHERE matchid = $1 AND team_number = $2", [matchid, teamNumber])
+        if (result.rows[0].count === 0) {
+          await client.query("INSERT INTO team_match_stat (team_number, matchid, data) VALUES ($1, $2, $3)", teamNumber, matchid, data) // need to INSERT a row
+        } if (result.rows[0].count === 1 ) {
+          await client.query("") // need to UPDATE a row
+        } else {
+          console.error("count is ${result.rows[0].count}")
+        }
+          response.json("action executed")
+      } finally {
+        client.release()
+      }
     })
     .catch(e => console.error(e.stack))
 })
